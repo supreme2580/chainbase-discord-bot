b5ead10917a3329da87fd8e79ff6fa6fc8ef544a
supreme2580 <victoromorogbe69@gmail.com>
2023-10-23 15:45:40 +0100

    chainbase-discord-bot

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post("/webhook", async (req, res) => {
   const { body } = req;
   const from = body.data.from_address;
   const to = body.data.to_address;
-  const value = parseInt(body.data.value, 18) || 0;
+  const value = Number(body.data.value/1000000000000000000) || 0;
 
   if ((from || to) === user_wallet.toLowerCase()) {
     try {
