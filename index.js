ef26195ebca7961eb75d9967c494d63ca18db4bd
supreme2580 <victoromorogbe69@gmail.com>
2023-10-23 03:29:55 +0100

    chainbase-discord-bot

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const user_wallet = "0xA3Db2Cb625bAe87D12AD769C47791a04BA1e5b29"
+const user_id = "919141293878280203"
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.login(process.env.PASS);
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
-    console.log('Received Webhook:', req.body);
-    res.status(200).send('OK');
+
+    const user = client.users.cache.get(userId);
+    const { body } = req
+    const from = body.data.from_address
+    const to = body.data.to_address
+    const value = Number(body.data.value / 1E18) || 0;
+
+    client.once("ready", () => {
+
+        if (user) {
+            const message = `Hey chief, you just ${from !== user_wallet ? `received ${value} Eth on Base from ${from}` : `sent ${value} Eth to ${to} on Base`}`;
+            user.send(message).then(() => console.log(`Message sent to ${user}`)).catch(e => console.log(e))
+            res.status(200).send('OK');
+        }
+        else {
+            console.log("User not found")
+        }
+
+    })
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-    console.log(`Webhook receiver listening on port ${PORT}`);
+    console.log(`Webhook receiver listening🎉🎉🎉`);
 });
