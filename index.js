403167d7ba0a969b3510148568b14ae50b3de4d1
supreme2580 <victoromorogbe69@gmail.com>
2023-10-23 05:29:50 +0100

    chainbase-discord-bot

--- index.js
+++ index.js
@@ -26,22 +26,17 @@ app.post("/webhook", async (req, res) => {
   const to = body.data.to_address;
   const value = Number(body.data.value / 1e18) || 0;
 
-  if (from == user_wallet || to == user_wallet) {
-    try {
-      const message = `Hey chief, you just ${
-        from !== user_wallet
-          ? `received ${value} Eth on Base from ${from}`
-          : `sent ${value} Eth to ${to} on Base`
-      }`;
-      user.send(message);
-      return res.status(200).json();
-    } catch (error) {
-      console.log(error);
-      return res.status(400).json();
-    }
-  }
-  else {
-    console.log("Transaction irrelevant to user")
+  try {
+    const message = `Hey chief, you just ${
+      from !== user_wallet
+        ? `received ${value} Eth on Base from ${from}`
+        : `sent ${value} Eth to ${to} on Base`
+    }`;
+    user.send(message);
+    return res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json();
   }
 });
 
